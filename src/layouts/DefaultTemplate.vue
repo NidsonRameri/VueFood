9e1fe8cc7dab519f424c879c1d08eebb98cfb9c6
nidson <nidson1@hotmail.com>
2020-09-21 21:05:43 -0300

    Começando VueFood

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        vuevue
+    </div>
+</template>
+
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+export default {
+    
+}
+</script>
+
+
+<style scoped>
+@import url('../assets/css/larafood.css');
+@import url('../assets/css/reset.css');
+</style> 
